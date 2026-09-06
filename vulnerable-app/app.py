ef7994b5cf2bb0f3c3ccfe9658ec960239a1a55a
Sampson Chinecherem Enyindah <126031463+SC-ENYINDAH@users.noreply.github.com>
2026-09-07 00:40:31 +0100

    Fixed Sqli vulnerability

--- vulnerable-app/app.py
+++ vulnerable-app/app.py
@@ -3,11 +3,11 @@ import sqlite3
 def get_user(user_input):
 
     conn = sqlite3.connect("users.db")
-
     cursor = conn.cursor()
 
-    query = f"SELECT * FROM users WHERE username = ?"
-
-    cursor.execute(query,(user_input,))
+    cursor.execute(
+        "SELECT * FROM users WHERE username = ?",
+        (user_input,)
+    )
 
     return cursor.fetchall()
